de55691871d75a44016d846c60814789fad8de11
oussama-benelhadj-djelloul <obenelhadjdjelloul1999@gmail.com>
2021-06-01 08:36:08 +0200

    Final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { useState } from 'react';
+import logo from './sad.jpg';
+import axios from 'axios';
+import { Container, Row, Col } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    location: '',
+    apiKey: '7721bf92f8decccfcb7e72704f8f58b1',
+    weather: '',
+    hide: '',
+    show: 'hidden',
+  }
+  handleChange = (event) => {
+    this.setState({ location: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://api.openweathermap.org/data/2.5/weather?q=' + this.state.location + '&units=imperial&appid=' + this.state.apiKey)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        console.log(res.data.main);
+        this.setState({ weather: res.data.main })
+        this.setState({ hide: "hidden", show: "" })
+      })
+  }
+
+  cancelHide = (event) => {
+    this.setState({ show: "hidden", hide: "" })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <Container fluid>
+          <Row>
+            <Col >
+
+              <div class="p-4 w-100 mx-auto text-center">
+
+
+                <img src={logo} class="w-100" height="900px" alt="" />
+              </div>
+            </Col>
+            <Col>
+              <div class="d-flex   align-items-center" >
+                <div class="w-75 px-5 text-center center">
+                  <h1 class="mx-auto">The Weather Chewaf</h1>
+                  <div class={this.state.hide}>
+
+
+                    <hr />
+                    <form onSubmit={this.handleSubmit}>
+                      <div class="form-group">
+                        <label >
+                          <h1 class="mb-3">
+                            Location Name
+                    </h1>
+                          <input type="text" name="location" class="w-100 w-100 formi" onChange={this.handleChange} />
+                        </label>
+                      </div>
+                      <button type="submit" class="btn btn-success">Choof it</button>
+                    </form>
+                    <hr />
+                  </div>
+                  <h1>
+                    <div className={this.state.show} >
+                      <h1 class="my-1">
+                        {this.state.location} :
+
+                      </h1>
+                      <h1 class="my-1 text-success">
+                        {(this.state.weather.temp - 32) * 5 / 9}
+
+                      </h1>
+                      <br />
+                      <button onClick={this.cancelHide} class="btn btn-primary">Search Again</button>
+                    </div>
+                  </h1>
+                </div>
+              </div>
+            </Col>
+
+          </Row>
+        </Container>
+      </div >
+    );
+
+  }
 }
 
 export default App;
